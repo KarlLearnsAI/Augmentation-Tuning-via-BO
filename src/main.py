65e18381b6b7230ebaac85d9b3dab8b41eb4f6e5
Karl Johannes <109764022+KarlLearnsAI@users.noreply.github.com>
2024-05-27 14:39:58 +0000

    added magnitude tuning

--- src/main.py
+++ src/main.py
@@ -7,13 +7,13 @@ import augmentations
 def main():
     img_paths = ['../data/class1.jpg', '../data/class2.jpg', '../data/class1.jpg', '../data/class4.jpg', '../data/class5.jpg']
     img_path = '../data/image.jpg'
-    augment_list = list(augmentations.augment_list())#[:20]  # Specify the range of augmentations you want to use
-    valid_augmentations = []
+    augment_list = list(augmentations.augment_list()[:])#[:20]  # Specify the range of augmentations you want to use
+
     print(f"Length of augmentations: {len(augment_list)}")
     
     for i in range(0, len(augment_list), 5):
         print(f"Current Range: {i}-{i+5}")
-        if len(augment_list) > i+5:
+        if len(augment_list) >= i+5:
             augment_group = augment_list[i:i+5]
         elif i+5 == len(augment_list):
             break
@@ -22,17 +22,40 @@ def main():
             augment_group = augment_list[i:len(augment_list)]
         print(f"Evaluating: {augment_group}")
         selector = AugmentationDeselector(img_paths, augment_group)
-        valid_augmentations_run = selector.run()
-        valid_augmentations.extend(valid_augmentations_run)
+        new_valid_augmentations, new_search_space = selector.run()
+        print(f"Received: {new_valid_augmentations} | {new_search_space}")
+        if i == 0:
+            valid_augmentations = new_valid_augmentations
+            entire_search_space = new_search_space
+            print(f"Current run: {new_search_space}")
+            print(f"Updated dict: {entire_search_space}")
+        else:
+            valid_augmentations.extend(new_valid_augmentations)
+            entire_search_space.update(new_search_space)
+            print(f"Current run: {new_search_space}")
+            print(f"Updated dict: {entire_search_space}")
+            
 
-    print("Selected augmentations:", valid_augmentations)
+    #print("Selected augmentations:", valid_augmentations)
+    #print(valid_augmentations)
+    
+    extracted_search_space = {}
+    for augmentation in valid_augmentations:
+        extracted_search_space[augmentation] = entire_search_space[augmentation]
+
+    print("Min_max_values for valid/unchecked augmentations:")
+    print(extracted_search_space)
+    
+    
+    #print(type(self.aug_min_max))
+    #print(self.aug_min_max['ShearX'])
     
     #image_path = "../data/image.jpg"
     #augmentation_names = get_augmentation_space()
     #print(augmentation_names)
     
-    starting_points = get_starting_points(valid_augmentations, img_path)
-    print(starting_points)
+    # starting_points = get_starting_points(valid_augmentations, img_path)
+    # print(starting_points)
 
 
 if __name__ == "__main__":
--- src/searchspaceshaping.py
+++ src/searchspaceshaping.py
@@ -1,24 +1,27 @@
 from PIL import Image, ImageTk
 import tkinter as tk
-from tkinter import Button, Checkbutton, Label
+from tkinter import Button, Checkbutton, Label, Scale, Entry
 import augmentations
 
 class AugmentationDeselector:
-    def __init__(self, img_paths, augmentations, augmentation_strength=0.75):
+    def __init__(self, img_paths, augmentations, augmentation_magnitude=0.75):
         self.img_paths = img_paths
-        self.augmentation_strength = augmentation_strength
-        self.augmentations = augmentations
+        self.augmentation_magnitude = augmentation_magnitude
         self.augment_dict = {fn.__name__: (fn, v1, v2) for fn, v1, v2 in augmentations}
+        self.aug_min_max = {}
         self.images = []
         self.invalid_augmentations = {}
-        self.checkboxes = {}
-        self.augmented_images = []
-        self.filtered_augmentations = []
+        self.unchecked_augmentations = {}
+        self.checkboxes = {}  # Initialize checkboxes attribute
+        self.sliders = {}     # Initialize sliders attribute
+        self.min_entries = {} # Initialize min_entries attribute
+        self.max_entries = {} # Initialize max_entries attribute
         self.current_page = 0
         self.num_columns = 5
         self.num_rows = 5
         self.load_images()
         self.apply_augmentations()
+
 
     def load_images(self):
         for img_path in self.img_paths:
@@ -32,10 +35,28 @@ class AugmentationDeselector:
         return augment_fn(img.copy(), level * (high - low) + low)
 
     def apply_augmentations(self):
+        self.augmented_images = []
         for augment_name in self.augment_dict.keys():
             for image in self.images:
-                cur_image = self.apply_augment(image, augment_name, self.augmentation_strength)
+                cur_image = self.apply_augment(image, augment_name, self.augmentation_magnitude)
                 self.augmented_images.append(cur_image)
+
+    def update_images(self, column):
+        augment_name = list(self.augment_dict.keys())[column]
+        level = self.sliders[column].get() / 100.0
+
+        start_index = column * self.num_rows
+        for j in range(self.num_rows):
+            img_index = start_index + j
+            original_image = self.images[j]
+            augmented_image = self.apply_augment(original_image, augment_name, level)
+            self.augmented_images[img_index] = augmented_image
+            self.display_image(column, j, augmented_image)
+
+    def display_image(self, column, row, image):
+        photo = ImageTk.PhotoImage(image.resize((self.image_width - 20, self.image_height - 20), Image.BICUBIC))
+        self.buttons[column][row].configure(image=photo)
+        self.buttons[column][row].image = photo
 
     def display_images(self, root, page):
         start_col = page * self.num_columns
@@ -46,27 +67,48 @@ class AugmentationDeselector:
 
         self.title_label.grid(row=0, column=0, columnspan=self.num_columns, sticky="n")
         if page < self.total_pages - 1:
-            self.next_button.grid(row=self.num_rows + 2, column=self.num_columns - 1, sticky="e")
-        self.submit_button.grid(row=self.num_rows + 2, columnspan=self.num_columns, sticky="n")
+            self.next_button.grid(row=self.num_rows + 4, column=self.num_columns - 1, sticky="e")
+        self.submit_button.grid(row=self.num_rows + 4, columnspan=self.num_columns, sticky="n")
 
+        self.buttons = {}
         for i in range(start_col, end_col):
             augment_name = list(self.augment_dict.keys())[i]
+            self.buttons[i] = []
             for j in range(self.num_rows):
                 img_index = i * self.num_rows + j
                 photo = ImageTk.PhotoImage(self.resized_images[img_index].resize((self.image_width - 20, self.image_height - 20), Image.BICUBIC))
                 button = Button(root, image=photo, command=lambda i=i: self.toggle_checkbox(i))
                 button.image = photo
                 button.grid(row=j + 1, column=i - start_col, padx=10, pady=1)
-            
+                self.buttons[i].append(button)
             checkbox = Checkbutton(root, command=lambda i=i: self.toggle_checkbox(i))
             checkbox.grid(row=self.num_rows + 1, column=i - start_col, sticky="s")
             self.checkboxes[i] = checkbox
+
+            slider = Scale(root, from_=0, to=100, orient="horizontal", command=lambda val, i=i: self.update_images(i))
+            slider.set(self.augmentation_magnitude * 100)
+            slider.grid(row=self.num_rows + 2, column=i - start_col, sticky="n")
+            self.sliders[i] = slider
+
+            # Add min and max entry fields
+            min_label = Label(root, text="Min.")
+            min_label.grid(row=self.num_rows + 3, column=i - start_col, sticky="e")
+            min_entry = Entry(root)
+            min_entry.insert(0, "0")
+            min_entry.grid(row=self.num_rows + 3, column=i - start_col, sticky="w")
+            self.min_entries[i] = min_entry
+
+            max_label = Label(root, text="Max.")
+            max_label.grid(row=self.num_rows + 4, column=i - start_col, sticky="e")
+            max_entry = Entry(root)
+            max_entry.insert(0, "100")
+            max_entry.grid(row=self.num_rows + 4, column=i - start_col, sticky="w")
+            self.max_entries[i] = max_entry
 
     def toggle_checkbox(self, column):
         start_index = column * self.num_rows
         end_index = min(start_index + self.num_rows, len(self.augmented_images))
         is_checked = all(self.invalid_augmentations.get(i, False) for i in range(start_index, end_index))
-        
         for i in range(start_index, end_index):
             self.invalid_augmentations[i] = not is_checked
 
@@ -82,9 +124,22 @@ class AugmentationDeselector:
             self.display_images(self.root, self.current_page)
 
     def submit(self):
-        # Collect unchecked augmentations
-        unchecked_augmentations = [list(self.augment_dict.keys())[i] for i in range(len(self.augment_dict)) if not any(self.invalid_augmentations.get(j, False) for j in range(i * self.num_rows, (i + 1) * self.num_rows))]
-        self.filtered_augmentations = unchecked_augmentations
+        self.unchecked_augmentations = [
+            list(self.augment_dict.keys())[i] 
+            for i in range(len(self.augment_dict)) 
+            if not any(self.invalid_augmentations.get(j, False) for j in range(i * self.num_rows, (i + 1) * self.num_rows))
+        ]
+
+        for i in range(len(self.augment_dict)):
+            augment_name = list(self.augment_dict.keys())[i]
+            min_value = float(self.min_entries[i].get()) / 100.0
+            max_value = float(self.max_entries[i].get()) / 100.0
+            self.aug_min_max[augment_name] = {'min': min_value, 'max': max_value}
+
+        self.filtered_augmentations = {
+            'unchecked_augmentations': self.unchecked_augmentations,
+            'min_max_values': self.aug_min_max
+        }
         self.root.destroy()
 
     def run(self):
@@ -93,25 +148,23 @@ class AugmentationDeselector:
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
         self.image_width = screen_width // self.num_columns
-        self.image_height = screen_height // (self.num_rows + 1)  # Add space for the checkbox
+        self.image_height = screen_height // (self.num_rows + 2)
         self.resized_images = [img.resize((self.image_width, self.image_height), Image.BICUBIC) for img in self.augmented_images]
 
-        self.title_label = Label(self.root, text="Check all invalid images", font=("Arial", 16, "bold"))
+        self.title_label = Label(self.root, text="Check all invalid augmentations/columns", font=("Arial", 16, "bold"))
         self.next_button = Button(self.root, text="Next", command=self.next_page)
         self.submit_button = Button(self.root, text="Submit", command=self.submit)
 
         self.display_images(self.root, self.current_page)
         self.root.mainloop()
-        return self.filtered_augmentations
-
-
+        return self.unchecked_augmentations, self.aug_min_max # self.filtered_augmentations
 
 def main():
     img_paths = ['../data/class1.jpg', '../data/class2.jpg', '../data/class1.jpg', '../data/class4.jpg', '../data/class5.jpg']
-    augment_list = list(augmentations.augment_list())[:5]  # Specify the range of augmentations you want to use
+    augment_list = list(augmentations.augment_list())[:5]
     selector = AugmentationDeselector(img_paths, augment_list)
     selected_augmentations = selector.run()
     print("Selected augmentations:", selected_augmentations)
-    
+
 if __name__ == "__main__":
     main()
